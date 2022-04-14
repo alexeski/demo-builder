bd331c754b6c78df4499ca49d553a951cbf0dd34
alexeski <8482762+alexeski@users.noreply.github.com>
2022-04-14 10:41:21 +0100

    v1.6.0, issues with images

--- build-it.sh
+++ build-it.sh
@@ -1,13 +1,23 @@
 #! /bin/bash
+
+#this is used to minify main.js to main.min.js
 node prepdeploy.js
+
+# this is used to commit the code to main branch in github
+# important! the best practice is to commit to a dev branch instead
 git add .
 git commit -m "v1.6.0, issues with images"
 git push
+
+# this is used 
 docker stop testemb 
 docker rm testemb 
-docker image rm alteirac/emb
-docker build . -t alteirac/emb
-docker run -p 80:3000 -d --name testemb alteirac/emb
-docker push alteirac/emb
+docker image rm alexswedocker/emb-demo-builder-eski
+docker build . -t alexswedocker/emb-demo-builder-eski
+docker run -p 80:3000 -d --name testemb alexswedocker/emb-demo-builder-eski
+
+docker push alexswedocker/emd-demo-builder-eski
+
+# this is to create the windows and mac apps (locally)
 npm run dist
 electron-builder --windows nsis:x64
